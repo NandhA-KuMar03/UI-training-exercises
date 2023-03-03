8d5652d671832a1706f94928339b4e6c797419a0
NandhA-KuMar03 <nandhakumar.kumaran@cdw.com>
2023-03-03 10:13:32 +0530

    Updated on the comments given

--- Friends_list/scripts/scripts.js
+++ Friends_list/scripts/scripts.js
@@ -1,22 +1,22 @@
 friends.forEach(element => {
-    let page_container = document.querySelector(".page_container");
+    let page_container = document.querySelector(".page-container");
     let card_container = document.createElement("div");
-    card_container.className = "card_container";
+    card_container.className = "card-container";
     let img_container = document.createElement("img");
     img_container.src = element.img;
-    img_container.className = "img_contain";
+    img_container.className = "img-contain";
     let details_container = document.createElement("div");
-    details_container.className = "details_container";
+    details_container.className = "details-container";
     let name = document.createElement("h3");
     name.className = "name";
     let mail = document.createElement("h6");
-    mail.className = "mailid";
+    mail.className = "mail-id";
     name.innerHTML = element.first_name + " " +element.last_name;
     mail.innerHTML = element.email;
 
-    card_container.appendChild(img_container);
-    card_container.appendChild(details_container);
+    card_container.appendChild(img-container);
+    card_container.appendChild(details-container);
     details_container.appendChild(name);
     details_container.appendChild(mail);
-    page_container.appendChild(card_container);
+    page_container.appendChild(card-container);
 });
