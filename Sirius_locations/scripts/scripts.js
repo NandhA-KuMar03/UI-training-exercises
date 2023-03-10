5b66a85167253b3171a82f2438c6544fcdf0b4ca
NandhA-KuMar03 <nandhakumar.kumaran@cdw.com>
2023-03-10 14:27:42 +0530

    Changed the accordion functionality

--- Sirius_locations/scripts/scripts.js
+++ Sirius_locations/scripts/scripts.js
@@ -1,67 +1,65 @@
 $(document).ready(function(){
-    $("#aboutUsButton").attr("autofocus" , true);
-    $("#aboutUsButton").click(function(){
-        $(".parent").empty();
+
+// The about us page when the page is loaded
+    function load(){
+        $(".parent *").remove();
         $(".button-container button").removeClass("active");
         $("#aboutUsButton").addClass("active");
-        let aboutUsDescription = $("<p></p>").text(about.description);
-        aboutUsDescription.addClass("about-us-description");
-        $(".parent").append(aboutUsDescription);
-        let aboutUsImageContainer = $("<div></div>").addClass("about-us-image-container");
-        let aboutUsImage = $("<img></img>").attr("src","../assets/images/aboutUs.png");
-        $(aboutUsImageContainer).append(aboutUsImage);
-        $(".parent").append(aboutUsImageContainer);
+        let $aboutUsContainer = $("<div>");
+        let $aboutUsDescription = $("<p></p>").text(about.description);
+        $aboutUsDescription.addClass("about-us-description");
+
+        // IMage in about us page
+        let $aboutUsImageContainer = $("<div></div>").addClass("about-us-image-container");
+        let $aboutUsImage = $("<img></img>").attr("src","../assets/images/aboutUs.png");
+        $aboutUsImageContainer.append($aboutUsImage);
+        $aboutUsContainer.append($aboutUsImageContainer , $aboutUsDescription)
+        $(".parent").prepend($aboutUsContainer);
+    }
+    load();
+
+    $("#aboutUsButton").click(function(){
+        load();
     });
 
+// The solution content when solution button is clicked
     $("#solutionButton").click(function(){
-        $(".parent").empty();
+        $(".parent *").remove();
         $(".button-container button").removeClass("active");
         $("#solutionButton").addClass("active");
-
-        
-        let $accordionContainer = $("<div>").addClass("accordion-container");
-        let $digitalExperienceContainer = $("<div>");
-        let $digitalExperienceTitle = $("<h5>").addClass("acc-slide-down");
+        let $acc = $("<div id='accordion'>");
+// Three accordion classes when solution button is clicked
+        let $digitalExperienceTitle = $("<h3>");
         $digitalExperienceTitle.text("DIGITAL EXPERIENCE");
         let $digitalExperienceDescription = $("<p>").addClass("accordion-para");
         $digitalExperienceDescription.text(accordion.digitalExperience);
 
-        let $informationTechnologycontainer = $("<div>");
-        let $informationTechnologyTitle = $("<h5>").addClass("acc-slide-down");
-        $informationTechnologyTitle.text("INFORMATION TECHNOLOGY");
-        let $informationTechnologyDescription = $("<p>").addClass("accordion-para");
+        let $informationTechnology = $("<h3>");
+        $informationTechnology.text("INFORMATION TECHNOLOGY");
+        let $informationTechnologyDescription= $("<p>").addClass("accordion-para");
         $informationTechnologyDescription.text(accordion.informationTechnology);
 
-        let $qualityAssuracneContainer = $("<div>");
-        let $qualityAssuracneTitle = $("<h5>").addClass("acc-slide-down");
-        $qualityAssuracneTitle.text("QUALITY ASSURANCE");
-        let $qualityAssuracneDescription = $("<p>").addClass("accordion-para");
-        $qualityAssuracneDescription.text(accordion.qualityAssuracne);
-
-        let $managedServicesContainer = $("<div>");
-        let $managedServicesTitle = $("<h5>").addClass("acc-slide-down");
-        $managedServicesTitle.text("MANAGED SERVICES");
+        let $managedServices = $("<h3>");
+        $managedServices.text("MANAGED SERVICES");
         let $managedServicesDescription = $("<p>").addClass("accordion-para");
         $managedServicesDescription.text(accordion.managedServices);
-
-        $digitalExperienceContainer.append($digitalExperienceTitle , $digitalExperienceDescription);
-        $informationTechnologycontainer.append($informationTechnologyTitle , $informationTechnologyDescription);
-        $qualityAssuracneContainer.append($qualityAssuracneTitle , $qualityAssuracneDescription);
-        $managedServicesContainer.append($managedServicesTitle , $managedServicesDescription);
-
-        $accordionContainer.append($digitalExperienceContainer , $informationTechnologycontainer,$qualityAssuracneContainer,$managedServicesContainer);
-        $(".parent").append($accordionContainer);
+        $acc.append($digitalExperienceTitle , $digitalExperienceDescription , $informationTechnology ,$informationTechnologyDescription , $managedServices ,$managedServicesDescription );
+        $(".parent").append($acc);
+        $('#accordion').accordion();
     });
     
-
+// This function runs when location button is clicked
     $("#locationButton").click(function(){
-        $(".parent").empty();
+        $(".parent *").remove();
+        let $locationsContainer = $("<div>");
         $(".button-container button").removeClass("active");
         $("#locationButton").addClass("active");
         var $table = $('<table/>').addClass("locations-table");
+        // The table tag for the list of locations 
         locations.forEach(element => {
             $table.append('<tr><td><img src="../assets/images/flag.png'  +  '"</img></td><td>' + element.state +  '</td><td>' + element.city + '</td><td>' + element.contact + '</td></tr>');
         });
-        $(".parent").append($table);
+        $locationsContainer.append($table);
+        $(".parent").prepend($locationsContainer);
     });
 });
